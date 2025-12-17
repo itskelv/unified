a5cbb168bb4b52295f7e098179a16936ec8bc474
itskelv <kelvinsusantogunawan@gmail.com>
2025-12-17 20:57:38 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
 params = {
-    'root_dir': '../unifieddataset',
-    'feat_dir': '../unifieddataset/features',
-    'norm_feat_dir': '../unifieddataset/norm_features',
-    'label_dir': '../unifieddataset/labels',
+    'root_dir': '../DCASE_FOA_DATASET',
+    'feat_dir': '../DCASE_FOA_DATASET/features',
+    'norm_feat_dir': '../DCASE_FOA_DATASET/norm_features',
+    'label_dir': '../DCASE_FOA_DATASET/labels',
     'unique_class': 13,
     'sampling_rate': 24000,
     'hop_len': 0.02,
