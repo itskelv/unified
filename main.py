2696b9d96a921a3cd673fe0780ac2aa7abce65a5
itskelv <kelvinsusantogunawan@gmail.com>
2026-01-11 16:11:05 +0800

    feature_extract

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import torch
-
-from parameters import params
 from extract_features import SELDFeatureExtractor
+from parameters import params
 
 def main():
     feature_extractor = SELDFeatureExtractor(params)
     feature_extractor.extract_features()
     feature_extractor.preprocess_features()
-    feature_extractor.extract_labels()
+
 
 if __name__ == '__main__':
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
