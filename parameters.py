29fa8efb993c82d256b2eb9860795067a1893777
itskelv <kelvinsusantogunawan@gmail.com>
2026-01-14 08:14:22 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -12,7 +12,22 @@ params = {
     'batch_size': 128,
     'label_sequence_length': 50,
     'model_dir': 'models/',
-    'f_pool_size': [4, 4, 2]  
+    'dropout_rate': 0.05,
+    'nb_cnn2d_filt': 64,  
+    'f_pool_size': [4, 4, 2],
+
+    'nb_heads': 8,
+    'nb_self_attn_layers': 2,
+    'nb_transformer_layers': 2,
+
+    'nb_rnn_layers': 2,
+    'rnn_size': 128,
+
+    'nb_fnn_layers': 1,
+    'fnn_size': 128,
+
+    'nb_epochs': 250,
+    'lr': 1e-3,
 }
 
 params['feature_label_resolution'] = int(params['label_hop_len'] // params['hop_len'])
