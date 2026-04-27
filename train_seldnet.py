e49d1b03788287ddd1e34b8ff6ed52189909c196
itskelv <kelvinsusantogunawan@gmail.com>
2026-04-27 11:23:27 +0800

    Update train_seldnet.py

--- train_seldnet.py
+++ train_seldnet.py
@@ -517,54 +517,67 @@ def main(argv):
             # ---------------------------------------------------------------------
             # Evaluate on unseen test data
             # ---------------------------------------------------------------------
+
+            modalities = ['foa', 'stereo']
             print('Load best model weights')
             model.load_state_dict(torch.load(model_name, map_location='cpu'))
 
-            print('Loading unseen test dataset:')
-            data_gen_test = cls_data_generator.DataGenerator(
-                params=params, split=test_splits[split_cnt], shuffle=False, per_file=True
-            )
+            for mod in modalities:
+                print("evaluating on", mod)
+                print('Loading unseen test dataset:')
+                data_gen_test = cls_data_generator.DataGenerator(
+                    params=params, split=test_splits[split_cnt], shuffle=False, per_file=True
+                )
+                
+                files = list(data_gen_test.get_filelist)
 
-            # Dump results in DCASE output format for calculating final scores
-            dcase_output_test_folder = os.path.join(params['dcase_output_dir'], '{}_{}_test'.format(unique_name, strftime("%Y%m%d%H%M%S", gmtime())))
-            cls_feature_class.delete_and_create_folder(dcase_output_test_folder)
-            print('Dumping recording-wise test results in: {}'.format(dcase_output_test_folder))
+                if mod == 'stereo':
+                    data_gen_test._filenames_list = [f for f in files if "deg" in f]
+                else:
+                    data_gen_test._filenames_list = [f for f in files if "deg" not in f]
+
+                print(len(data_gen_test._filenames_list))
+
+                # Dump results in DCASE output format for calculating final scores
+                dcase_output_test_folder = os.path.join(params['dcase_output_dir'], '{}_{}_test'.format(unique_name, strftime("%Y%m%d%H%M%S", gmtime())))
+                cls_feature_class.delete_and_create_folder(dcase_output_test_folder)
+                print('Dumping recording-wise test results in: {}'.format(dcase_output_test_folder))
 
 
-            test_loss = test_epoch(data_gen_test, model, criterion, dcase_output_test_folder, params, device)
+                test_loss = test_epoch(data_gen_test, model, criterion, dcase_output_test_folder, params, device)
 
-            use_jackknife=True
-            test_ER, test_F, test_LE, test_dist_err, test_rel_dist_err, test_LR, test_seld_scr, classwise_test_scr = score_obj.get_SELD_Results(dcase_output_test_folder, is_jackknife=use_jackknife )
+                use_jackknife=True
+                test_ER, test_F, test_LE, test_dist_err, test_rel_dist_err, test_LR, test_seld_scr, classwise_test_scr = score_obj.get_SELD_Results(dcase_output_test_folder, is_jackknife=use_jackknife )
 
-            print('SELD score (early stopping metric): {:0.2f} {}'.format(test_seld_scr[0] if use_jackknife else test_seld_scr, '[{:0.2f}, {:0.2f}]'.format(test_seld_scr[1][0], test_seld_scr[1][1]) if use_jackknife else ''))
-            print('SED metrics: F-score: {:0.1f} {}'.format(100* test_F[0]  if use_jackknife else 100* test_F, '[{:0.2f}, {:0.2f}]'.format(100* test_F[1][0], 100* test_F[1][1]) if use_jackknife else ''))
-            print('DOA metrics: Angular error: {:0.1f} {}'.format(test_LE[0] if use_jackknife else test_LE, '[{:0.2f} , {:0.2f}]'.format(test_LE[1][0], test_LE[1][1]) if use_jackknife else ''))
-            print('Distance metrics: {:0.2f} {}'.format(test_dist_err[0] if use_jackknife else test_dist_err, '[{:0.2f} , {:0.2f}]'.format(test_dist_err[1][0], test_dist_err[1][1]) if use_jackknife else ''))
-            print('Relative Distance metrics: {:0.2f} {}'.format(test_rel_dist_err[0] if use_jackknife else test_rel_dist_err, '[{:0.2f} , {:0.2f}]'.format(test_rel_dist_err[1][0], test_rel_dist_err[1][1]) if use_jackknife else ''))
+                print('SELD score (early stopping metric): {:0.2f} {}'.format(test_seld_scr[0] if use_jackknife else test_seld_scr, '[{:0.2f}, {:0.2f}]'.format(test_seld_scr[1][0], test_seld_scr[1][1]) if use_jackknife else ''))
+                print('SED metrics: F-score: {:0.1f} {}'.format(100* test_F[0]  if use_jackknife else 100* test_F, '[{:0.2f}, {:0.2f}]'.format(100* test_F[1][0], 100* test_F[1][1]) if use_jackknife else ''))
+                print('DOA metrics: Angular error: {:0.1f} {}'.format(test_LE[0] if use_jackknife else test_LE, '[{:0.2f} , {:0.2f}]'.format(test_LE[1][0], test_LE[1][1]) if use_jackknife else ''))
+                print('Distance metrics: {:0.2f} {}'.format(test_dist_err[0] if use_jackknife else test_dist_err, '[{:0.2f} , {:0.2f}]'.format(test_dist_err[1][0], test_dist_err[1][1]) if use_jackknife else ''))
+                print('Relative Distance metrics: {:0.2f} {}'.format(test_rel_dist_err[0] if use_jackknife else test_rel_dist_err, '[{:0.2f} , {:0.2f}]'.format(test_rel_dist_err[1][0], test_rel_dist_err[1][1]) if use_jackknife else ''))
 
-            if params['average']=='macro':
-                print('Classwise results on unseen test data')
-                print('Class\tF\tAE\tdist_err\treldist_err\tSELD_score')
-                for cls_cnt in range(params['unique_classes']):
-                    print('{}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}'.format(
-                        cls_cnt,
+                if params['average']=='macro':
+                    print('Classwise results on unseen test data')
+                    print('Class\tF\tAE\tdist_err\treldist_err\tSELD_score')
+                    for cls_cnt in range(params['unique_classes']):
+                        print('{}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}'.format(
+                            cls_cnt,
 
-                        classwise_test_scr[0][1][cls_cnt] if use_jackknife else classwise_test_scr[1][cls_cnt],
-                        '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][1][cls_cnt][0],
-                                                    classwise_test_scr[1][1][cls_cnt][1]) if use_jackknife else '',
-                        classwise_test_scr[0][2][cls_cnt] if use_jackknife else classwise_test_scr[2][cls_cnt],
-                        '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][2][cls_cnt][0],
-                                                    classwise_test_scr[1][2][cls_cnt][1]) if use_jackknife else '',
-                        classwise_test_scr[0][3][cls_cnt] if use_jackknife else classwise_test_scr[3][cls_cnt],
-                        '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][3][cls_cnt][0],
-                                                    classwise_test_scr[1][3][cls_cnt][1]) if use_jackknife else '',
-                        classwise_test_scr[0][4][cls_cnt] if use_jackknife else classwise_test_scr[4][cls_cnt],
-                        '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][4][cls_cnt][0],
-                                                    classwise_test_scr[1][4][cls_cnt][1]) if use_jackknife else '',
+                            classwise_test_scr[0][1][cls_cnt] if use_jackknife else classwise_test_scr[1][cls_cnt],
+                            '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][1][cls_cnt][0],
+                                                        classwise_test_scr[1][1][cls_cnt][1]) if use_jackknife else '',
+                            classwise_test_scr[0][2][cls_cnt] if use_jackknife else classwise_test_scr[2][cls_cnt],
+                            '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][2][cls_cnt][0],
+                                                        classwise_test_scr[1][2][cls_cnt][1]) if use_jackknife else '',
+                            classwise_test_scr[0][3][cls_cnt] if use_jackknife else classwise_test_scr[3][cls_cnt],
+                            '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][3][cls_cnt][0],
+                                                        classwise_test_scr[1][3][cls_cnt][1]) if use_jackknife else '',
+                            classwise_test_scr[0][4][cls_cnt] if use_jackknife else classwise_test_scr[4][cls_cnt],
+                            '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][4][cls_cnt][0],
+                                                        classwise_test_scr[1][4][cls_cnt][1]) if use_jackknife else '',
 
-                        classwise_test_scr[0][6][cls_cnt] if use_jackknife else classwise_test_scr[6][cls_cnt],
-                        '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][6][cls_cnt][0],
-                                                    classwise_test_scr[1][6][cls_cnt][1]) if use_jackknife else ''))
+                            classwise_test_scr[0][6][cls_cnt] if use_jackknife else classwise_test_scr[6][cls_cnt],
+                            '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][6][cls_cnt][0],
+                                                        classwise_test_scr[1][6][cls_cnt][1]) if use_jackknife else ''))
 
     if params['mode'] == 'eval':
 
